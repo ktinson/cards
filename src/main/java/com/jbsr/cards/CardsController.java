9ddde90263a17606ddedfcda2fec76c9b8160462
ktinson <monikai.tinson@verizon.com>
2024-10-24 15:21:55 -0500

    main

--- src/main/java/com/jbsr/cards/CardsController.java
+++ src/main/java/com/jbsr/cards/CardsController.java
@@ -1,16 +1,21 @@
 package com.jbsr.cards;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/cards")
 public class CardsController {
+    @Autowired
+    private CardService cardService;
     @GetMapping
-    public ResponseEntity<String> allCards(){
-        return new ResponseEntity<String>("All Cards", HttpStatus.OK);
+    public ResponseEntity<List<LargeCard>> allCards(){
+        return new ResponseEntity<List<LargeCard>>(cardService.getAllCards(), HttpStatus.OK);
     }
 }
