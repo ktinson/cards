25affc8876941791828c6c84f88bfcfba8490874
ktinson <monikai.tinson@verizon.com>
2024-10-30 08:50:15 -0500

    Needed to update backend src

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 let apiURL;
 
 if (process.env.NODE_ENV === "development") {
-  apiURL = process.env.REACT_APP_API_URL || "http://localhost:8080"
+  apiURL = process.env.REACT_APP_API_URL || "https://cards-backend-1hpn.onrender.com"
 } else {
-  apiURL = "/api";
+  apiURL = "https://cards-backend-1hpn.onrender.com";
 }
 export default apiURL;
